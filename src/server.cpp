ade56a9b922d0e2be34eaaa379247aa82ec279ad
abdurisaq <abdurisaq@cs.sfu.ca>
2024-07-23 09:43:58 -0700

    respond with 200

--- src/server.cpp
+++ src/server.cpp
@@ -53,10 +53,16 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  if (client_fd < 0) {
+   std::cerr << "Failed to connect to client\n";
+   return 1;
+  }
   
+  std::cout << "Client connected\n";
+  std::string response = "HTTP/1.1 200 OK\r\n\r\n";
+  send(client_fd,response.c_str(),sizeof(response),0);
   close(server_fd);
-
+  
   return 0;
 }
